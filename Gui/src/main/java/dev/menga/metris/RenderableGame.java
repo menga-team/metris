232c0c2f7fa39978ab758f293415687391bba961
emilmair <emil.mair@posteo.net>
2025-06-22 16:26:40 +0200

    connected textures

--- Gui/src/main/java/dev/menga/metris/RenderableGame.java
+++ Gui/src/main/java/dev/menga/metris/RenderableGame.java
@@ -13,6 +13,28 @@ public class RenderableGame extends Game {
     static final int FIELD_RENDER_UNIT = 32;
     static final int NEXT_TETROMINOS = 4;
 
+    static final int[] TEXTURE_LOOKUP = {
+            1, 5, 1, 5, 7, 13, 7, 9, 1, 5, 1, 5, 7, 13, 7, 9, 4, 3, 4, 3, 12, 31, 12, 24, 4, 3, 4, 3, 8, 25, 8, 19, 1,
+            5, 1, 5, 7, 13, 7, 9, 1, 5, 1, 5, 7, 13, 7, 9, 4, 3, 4, 3, 12, 31, 12, 24, 4, 3, 4, 3, 8, 25, 8, 19, 6,
+            15, 6, 15, 2, 29, 2, 23, 6, 15, 6, 15, 2, 29, 2, 23, 14, 30, 14, 30, 28, 46, 28, 44, 14, 30, 14, 30, 20,
+            45, 20, 39, 6, 15, 6, 15, 2, 29, 2, 23, 6, 15, 6, 15, 2, 29, 2, 23, 10, 27, 10, 27, 21, 43, 21, 41, 10,
+            27, 10, 27, 16, 37, 16, 35, 1, 5, 1, 5, 7, 13, 7, 9, 1, 5, 1, 5, 7, 13, 7, 9, 4, 3, 4, 3, 12, 31, 12, 24,
+            4, 3, 4, 3, 8, 25, 8, 19, 1, 5, 1, 5, 7, 13, 7, 9, 1, 5, 1, 5, 7, 13, 7, 9, 4, 3, 4, 3, 12, 31, 12, 24,
+            4, 3, 4, 3, 8, 25, 8, 19, 6, 11, 6, 11, 2, 22, 2, 17, 6, 11, 6, 11, 2, 22, 2, 17, 14, 26, 14, 26, 28, 42,
+            28, 36, 14, 26, 14, 26, 20, 40, 20, 34, 6, 11, 6, 11, 2, 22, 2, 17, 6, 11, 6, 11, 2, 22, 2, 17, 10, 18,
+            10, 18, 21, 38, 21, 32, 10, 18, 10, 18, 16, 33, 16, 0};
+
+/*
+        block texturing process
+        +------------+-------------------------------+-------------------+-----------------------+-------------------+
+        |neighbors   |   bit-array                   |              int  |   lookup table        |   texture index   |
+        |            |                               |                   |                       |                   |
+        |0 1 1       |                               |                   |                       |                   |
+        |0 x 0       |   [0, 1, 1, 0, 1, 1, 1, 0]    |   110             |   array[110]          |   2               |
+        |1 1 1       |                               |                   |                       |                   |
+        +------------+-------------------------------+-------------------+-----------------------+-------------------+
+*/
+
     private Resources resources;
     private final Vec2i gridOff;
     private final Vec2i nextOff;
@@ -34,11 +56,51 @@ public class RenderableGame extends Game {
         for (int y = 0; y < Field.MAX_VISIBLE_HEIGHT; ++y) {
             for (int x = 0; x < Field.MAX_WIDTH; ++x) {
                 Color color = this.field.getColors()[y][x];
-                // TODO: Maybe rethink visible sprites and keep the void
-                // a normal sprite as any other color/garbage?
                 batch.draw(this.resources.getUnit(), this.gridOff.getX() + (FIELD_RENDER_UNIT * x), this.gridOff.getY() + (FIELD_RENDER_UNIT * y));
                 if (color.isVisible()) {
-                    batch.draw(this.resources.getColor(color), this.gridOff.getX() + (FIELD_RENDER_UNIT * x), this.gridOff.getY() + (FIELD_RENDER_UNIT * y));
+
+                    int[] inp = new int[8];
+                    for (int i = 0; i < 8; i++) {
+                        inp[i] = 0;
+                    }
+                    int ly = this.field.MAX_VISIBLE_HEIGHT-1;
+                    int lx = this.field.MAX_WIDTH-1;
+
+                    if (x > 0 && y < ly && this.field.getAt(Vec2i.of(x - 1, y + 1)).equals(color)) {
+                        inp[0] = 1;
+                    }
+                    if (y < ly && this.field.getAt(Vec2i.of(x, y + 1)).equals(color)) {
+                        inp[1] = 1;
+                    }
+                    if (x < lx && y < ly && this.field.getAt(Vec2i.of(x + 1, y + 1)).equals(color)) {
+                        inp[2] = 1;
+                    }
+                    if (x < lx && this.field.getAt(Vec2i.of(x + 1, y)).equals(color)) {
+                        inp[3] = 1;
+                    }
+                    if (x < lx && y > 0 && this.field.getAt(Vec2i.of(x + 1, y - 1)).equals(color)) {
+                        inp[4] = 1;
+                    }
+                    if (y > 0 && this.field.getAt(Vec2i.of(x, y - 1)).equals(color)) {
+                        inp[5] = 1;
+                    }
+                    if (x > 0 && y > 0 && this.field.getAt(Vec2i.of(x - 1, y - 1)).equals(color)) {
+                        inp[6] = 1;
+                    }
+                    if (x > 0 && this.field.getAt(Vec2i.of(x - 1, y)).equals(color)) {
+                        inp[7] = 1;
+                    }
+
+                    int result = 0;
+                    for (int i = 0; i < 8; i++) {
+                        result = (result << 1) | inp[i];
+                    }
+
+                    int index = TEXTURE_LOOKUP[result];
+
+                    Metris.getLogger().info("{} {} {}", inp, result, index);
+
+                    batch.draw(this.resources.getTile(color, index), this.gridOff.getX() + (FIELD_RENDER_UNIT * x), this.gridOff.getY() + (FIELD_RENDER_UNIT * y));
                 }
             }
         }
@@ -77,7 +139,15 @@ public class RenderableGame extends Game {
         if (ghost) {
             texture = this.resources.getGhostColor(toRender.getColor());
         } else {
-            texture = this.resources.getColor(toRender.getColor());
+
+
+
+
+
+
+
+
+            texture = this.resources.getTile(toRender.getColor(), 0);
         }
 
         for (Vec2i tile : toRender.getShape().getTiles()) {
--- Gui/src/main/java/dev/menga/metris/Resources.java
+++ Gui/src/main/java/dev/menga/metris/Resources.java
@@ -3,6 +3,7 @@ package dev.menga.metris;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Disposable;
@@ -12,18 +13,24 @@ public class Resources implements Disposable {
 
     private final AssetManager assetManager = new AssetManager();
     private final TextureAtlas atlas;
+    private final TextureAtlas tileAtlas;
 
     @Getter
     final private TextureRegion unit;
     final private TextureRegion[] colorTextures;
-    final private TextureRegion[] ghostColorTextures;
+    final private TextureRegion[] tileTextures;
 
+    final private TextureRegion[] ghostColorTextures;
 
     Resources() {
         this.assetManager.load("textures.atlas", TextureAtlas.class);
+        this.assetManager.load("tiles.atlas", TextureAtlas.class);
+
         this.assetManager.finishLoading();
         this.atlas = assetManager.get("textures.atlas", TextureAtlas.class);
+        this.tileAtlas = assetManager.get("tiles.atlas", TextureAtlas.class);
         this.colorTextures = this.atlas.findRegion("colors").split(32, 32)[0];
+        this.tileTextures = this.tileAtlas.findRegion("everything").split(32, 32)[0];
         this.ghostColorTextures = this.atlas.findRegion("colors").split(32, 32)[1];
         this.unit = this.atlas.findRegion("unit");
     }
@@ -32,8 +39,8 @@ public class Resources implements Disposable {
         return atlas.findRegion(texture);
     }
 
-    public TextureRegion getColor(Color color) {
-        return this.colorTextures[color.getId()];
+    public TextureRegion getTile(Color color, int index) {
+        return this.tileTextures[index];
     }
 
     public TextureRegion getGhostColor(Color color) {
