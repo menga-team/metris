b2af9b7f2219b0fc9d29833ccadd4bec0e41197c
Vladislav Yegorov <vlad@yegorov.org>
2025-06-22 08:04:17 +0200

    add emacs config

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil .
+      ((indent-tabs-mode . nil)
+       (fill-column . 80)
+       (compile-command . "mvn package --color always && java -jar Gui/target/METRIS-full.jar"))))
